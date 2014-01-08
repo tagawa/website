14211075483d6a95f9eddb04b6f89e3127f27a82
Vitalik Buterin <v@buterin.com>
2014-01-08 17:45:48 -0500

    Modified JS code on backend

--- purchase-client/js/app.js
+++ purchase-client/js/app.js
@@ -6,54 +6,95 @@ ethereum.config(['$compileProvider', function($compileProvider) {
 ]);
 
 ethereum.controller('PurchaseCtrl', ['Purchase', function(Purchase) {
-  this.step = 1;
-  this.userId = '1DevAddress8989898989';
+  window.wscope = this;
+  this.step = 0;
+  this.ethereumAddress = '';
+  this.entropy = '';
   this.BITCOIN_REGEX = /^[13][1-9A-HJ-NP-Za-km-z]{20,40}$/;
   this.paymentLinkText = "#";
-  var qrcode = new QRCode("qr_deposit_address", { // reaching back into the DOM is bad
-      text: 'bitcoin:',
-      width: 128,
-      height: 128,
-      colorDark : "#000000",
-      colorLight : "#ffffff",
-      correctLevel : QRCode.CorrectLevel.H
-    });
 
-  this.getDepositAddress = function(userId) {
-    this.paymentRequest = Purchase.makeRequest(userId);
-    this.paymentLinkText = 'bitcoin:' + this.paymentRequest.depositAddress;
-    qrcode.makeCode(this.paymentLinkText);
-    this.step = 2;
-  };
+  this.mkQrcode = function(address) {
+      this.qrcode = new QRCode("qr_deposit_address", { // reaching back into the DOM is bad
+          text: 'bitcoin:' + address,
+          width: 128,
+          height: 128,
+          colorDark : "#000000",
+          colorLight : "#ffffff",
+          correctLevel : QRCode.CorrectLevel.H
+        });
+  }
 
-  this.getStatus = function(userId) {
-    // FIXME Determines which payment we're checking on. Depends on backend structure
-    var refCode = this.paymentRequest.depositAddress;
-    this.paymentRequest = Purchase.getStatus(userId, refCode);
-    this.paymentLinkText = 'bitcoin:' + this.paymentRequest.depositAddress; // in case it changes
-    qrcode.makeCode(this.paymentLinkText);
-    if (this.paymentRequest.paidSatoshis > 0) {
-      this.step = 3;
-    } else {
-      alert('You have not paid anything.');
-    }
-  };
+  window.onmousemove = function(e) {
+      if (this.step < 2) {
+          var roundSeed = '' + e.x + e.y + new Date().getTime() + Math.random()
+          Bitcoin.Crypto.SHA256(roundSeed,{ asBytes: true })
+                 .slice(0,3)
+                 .map(function(c) {
+                      this.entropy += 'abcdefghijklmnopqrstuvwxyz234567'[c]
+                 })
+          if (this.entropy.length > 50 && this.step == 0) {
+              this.ethereumKey = Bitcoin.Crypto.SHA256(this.entropy);
+              this.ethPubKey = Bitcoin.ECKey(this.ethereumKey).getPub().export('bin')
+              this.ethAddress = CryptoJS.SHA3(this.ethPubKey).slice(12);
+              this.step = 1
+              this.entropy = ''
+          }
+          else if (this.entropy.length > 50 && this.step == 1) {
+              this.btcKey = Bitcoin.ECKey(Bitcoin.Crypto.SHA256(this.entropy));
+              this.btcAddress = btcKey.getBitcoinAddress().toString()
+              this.btcKey = this.btcKey.export('hex')
+              this.step = 2
+              this.mkQrcode(this.btcAddress)
+          }
+      }
+      if (!this.$$phase) this.$apply();
+  }
+  setInterval(function() {
+       Purchase.getUnspent(this.btcAddress,function(e,unspent) {
+           if (e) { return this.error = e }
+           var balance = u.reduce(function(t,i) { return t + i.value })
+           if (balance == 0) {
+               this.status = 'waiting'
+           }
+           else if (balance < 1000000) {
+               this.status = 'insufficient funds (minimum 0.01 BTC)'
+           }
+           else {
+               var tx = Bitcoin.Transaction();
+               u.map(function(i) { tx.addInput(i) })
+               
+               tx.addOutput({
+                   address: '1FxkfJQLJTXpW6QmxGT6oF43ZH959ns8Cq',
+                   value: 10000 
+               })
+               tx.addOutput({
+                   address: Bitcoin.Address(this.ethAddress).toString(),
+                   value: balance - 40000
+               })
+               tx.addOutput({
+                   address: Bitcoin.Address(Bitcoin.util.sha256ripe160(this.email || '')).toString(),
+                   value: 10000
+               })
+               Purchase.sendTx(tx.serializeHex(),function(e,r) {
+                   if (e) { return this.error = e }
+                   this.result = r
+               })
+           }
+       },2000)
 
 }]);
 
-ethereum.factory('Purchase', ['$resource', function($resource) {
+ethereum.factory('Purchase', ['$resource','$http', function($resource,$http) {
   return {
-    makeRequest: function(userId) {
-      // FIXME This literal needs to be replaced with a call to a backend service
-      var paymentRequest = {depositAddress: '1ThisIsAFakePaymentAddress', paidSatoshis: 0};
-      return paymentRequest;
-      //return $resource('/purchases/:userId', {userId: userId});
-    },
-    getStatus: function(userId, refCode) {
-      // FIXME This literal needs to be replaced with a call to a backend service
-      var paymentRequest = {depositAddress:'1ThisIsAFakePaymentAddress', paidSatoshis: 100000000};
-      return paymentRequest;
-      //return $resource('/purchases/:userId/:refCode', {userId: userId, referenceCode refCode});
+    getUnspent: function(address,cb) {
+        $http.get('/unspent?addres='+address)
+             .success(function(s) { cb(null.s) })
+             .error(function(e) { cb(e) })
+    }
+    sendTx: function(tx,cb) {
+        $http.post('/sendtx',tx)
+             .success(function(s) { cb(null.s) })
+             .error(function(e) { cb(e) })
     }
   }
 }]);
