6427b336cbc9e5f6f1959f36a7d675cb6eb0d7d3
Taylor Gerring <taylor.gerring@gmail.com>
2014-01-16 13:32:00 -0600

    Added password meter
    TODO Strength algorithm needs improvement

--- assets/static/js/app.js
+++ assets/static/js/app.js
@@ -19,6 +19,84 @@ ethereum.directive('match',['$parse', function ($parse) {
   };
 }]);
 
+
+ethereum.directive('checkStrength', function () {
+  return {
+    replace: false,
+    restrict: 'EACM',
+    scope: { model: '=checkStrength' },
+    link: function (scope, element, attrs) {
+      
+      var strength = {
+        colors: ['#F00', '#F90', '#FF0', '#9F0', '#0F0'],
+        measureStrength: function (p) {
+          var _force = 0;                    
+          var _regex = /[$-/:-?{-~!"^_`\[\]]/g; //" (Commentaire juste là pour pas pourrir la coloration sous Sublime...)
+                                
+          var _lowerLetters = /[a-z]+/.test(p);                    
+          var _upperLetters = /[A-Z]+/.test(p);
+          var _numbers = /[0-9]+/.test(p);
+          var _symbols = _regex.test(p);
+                                
+          var _flags = [_lowerLetters, _upperLetters, _numbers, _symbols];                    
+          //var _passedMatches = $.grep(_flags, function (el) { return el === true; }).length;                                          
+          var _passedMatches = _flags.map(function (el) { return el === true; });
+          _matches = 0;
+          for (var i = 0; i < _passedMatches.length; i++) {
+            if (_passedMatches[i])
+              _matches += 1;
+          };
+          _force += 2 * p.length + ((p.length >= 10) ? 1 : 0);
+          _force += _matches * 10;
+              
+          // penality (short password)
+          _force = (p.length <= 6) ? Math.min(_force, 10) : _force;                                      
+          
+          // penality (poor variety of characters)
+          _force = (_matches == 1) ? Math.min(_force, 10) : _force;
+          _force = (_matches == 2) ? Math.min(_force, 20) : _force;
+          _force = (_matches == 3) ? Math.min(_force, 40) : _force;
+          
+          return _force;
+
+        },
+        getColor: function (s) {
+
+          var idx = 0;
+          if (s <= 10) { idx = 0; }
+          else if (s <= 20) { idx = 1; }
+          else if (s <= 30) { idx = 2; }
+          else if (s <= 40) { idx = 3; }
+          else { idx = 4; }
+
+          return { idx: idx + 1, col: this.colors[idx] };
+
+        }
+      };
+
+      scope.$watch('model', function (newValue, oldValue) {
+        if (!newValue || newValue === '') {
+          element.css({ "display": "none"  });
+        } else {
+          var c = strength.getColor(strength.measureStrength(newValue));
+          console.log(c);
+          element.css({ "display": "inline" });
+          var kids = element.children('li');
+
+          for (var i = 0; i < kids.length; i++) {
+            if (i < c.idx)
+              kids[i].style.backgroundColor = c.col;
+            else
+              kids[i].style.backgroundColor = '#DDD';
+          }
+        }
+      });
+  
+    },
+    template: '<li class="point"></li><li class="point"></li><li class="point"></li><li class="point"></li><li class="point"></li>'
+  };
+});
+
 ethereum.controller('PurchaseCtrl', ['Purchase','$scope', function(Purchase, $scope) {
   window.wscope = $scope;
   $scope.entropy = '';
@@ -95,7 +173,7 @@ ethereum.controller('PurchaseCtrl', ['Purchase','$scope', function(Purchase, $sc
         })
       }
     })
-  },5000)
+  },3000)
 
 }]);
 
